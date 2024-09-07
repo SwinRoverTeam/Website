51a37eb122292f69c080dbc25d710ba5c8597768
Rudra Sekhri <37140269+rudrathegreat@users.noreply.github.com>
2024-09-07 22:22:45 +1000

    Added more components
    Added right button, left button and hero slideshow components. Also refactored some stuff

---
+++ swin_rover_app/src/components/roundbutton/leftroundbutton.jsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import './roundbutton.css'
+import linkIconBlack from '../../assets/left-icon-black.svg'
+import linkIconWhite from '../../assets/left-icon-white.svg'
+
+function LeftRoundButton({ text, style, onClick}) {
+    return (
+      <a className="round-button" style={style} onClick={onClick}>
+        <div className="button-info">
+          <div className="button-icon-container left">
+            <img className="button-icon" src={linkIconBlack} alt="Link Icon Black" />
+            <img className="button-icon" src={linkIconWhite} alt="Link Icon White" />
+          </div>
+          <div className="button-text-container">
+            <p className="button-text">{text}</p>
+            <p className="button-text">{text}</p>
+          </div>
+        </div>
+      </a>
+    );
+  }
+
+export default LeftRoundButton;
