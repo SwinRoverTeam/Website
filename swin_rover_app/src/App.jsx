bd55964c2638a6805402dfa096546b7abf64f53f
Rudra Sekhri <37140269+rudrathegreat@users.noreply.github.com>
2024-09-08 15:21:59 +1000

    Update App.jsx
    Please do not change App.jsx

--- swin_rover_app/src/App.jsx
+++ swin_rover_app/src/App.jsx
@@ -1,39 +1,7 @@
-<<<<<<< HEAD
 import './App.css'
-import Footer from './components/footer/footer'
-import Sponser from './components/sponsers/sponsers'
 
 function App() {
-	return (
-		<>
-			<Sponser />
-			<Footer />
-			
-		</>
-	)
+	return ();
 }
 
-export default App
-=======
-import './App.css'
-import Navbar from './components/navbar/navbar'
-// import Footer from './components/footer/footer'
-
-// function App() {
-// 	return (
-// 		<>
-// 			<Footer />
-// 		</>
-// 	)
-// }
-
-function App() {
-	return (
-		<>
-			<Navbar />
-		</>
-	)
-}
-
-export default App
->>>>>>> 5fc027c0e88af46da36f07713cbdd63524fdf0cd
+export default App;
