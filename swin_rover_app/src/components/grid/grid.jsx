6beb924311a07015d2135738963d12846cfbef1b
zooxii <146405182+zooxii@users.noreply.github.com>
2024-09-09 15:16:20 +1000

    updage grid CSS

--- swin_rover_app/src/components/grid/grid.jsx
+++ swin_rover_app/src/components/grid/grid.jsx
@@ -2,23 +2,18 @@ import * as React from 'react';
 import './grid.css'
 
 function Grid({image_url, image_alt, title, subtext, button_url, button_name}) {
-    return (  
+    return (
+      /* when calls the component, add <div className="grid"> to wrap multiples Grid on a row */
       <div class="container">
-       
-          <section class="content">
-            <img src={image_url} alt={image_alt}></img>
-            <div className="span_text_and_button">
-              <div className="text_wrapper">
-                <h3 className="inter-paragraph">{title}</h3>
-                <p className="inter-links">{subtext}</p>
-            </div>
-              <a href={button_url} >
-                <button className="inter-links">{button_name}</button>
-              </a>
-            </div>
-          </section>
-     
-    </div>
+        <img src={image_url} alt={image_alt}></img>
+        <div className="span_text_and_button">
+          <div className="text_wrapper">
+            <h3 className="inter-paragraph">{title}</h3>
+            <p className="inter-links">{subtext}</p>
+          </div>
+            <a href={button_url}><button className="inter-links">{button_name}</button></a>
+          </div>
+      </div>
   );
 }
   export default Grid;
