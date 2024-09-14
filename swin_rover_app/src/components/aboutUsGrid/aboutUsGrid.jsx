efc5decb4d01ab5b8847514a6e05ced984dd1b78
toranbehera <toranbehera@gmail.com>
2024-09-14 23:37:29 +0700

    fixed grid2 issue

--- swin_rover_app/src/components/aboutUsGrid/aboutUsGrid.jsx
+++ swin_rover_app/src/components/aboutUsGrid/aboutUsGrid.jsx
@@ -1,34 +1,45 @@
 import Typography from '@mui/material/Typography';
 import { Box } from '@mui/material';
-import Image from '../../assets/Rectangle 17.png';
 import { PieChart } from '@mui/x-charts/PieChart';
-import Grid from '@mui/material/Grid';
+import Grid from '@mui/material/Grid2';
+import Button from '../roundbutton/roundbutton';
+import Image from '../../assets/Rectangle 17.png';
+import { positions } from '@mui/system';
 
 export default function About(){
     return(
         <>
             <Grid container spacing={1.6} margin={4}>
-               <Grid container spacing={1} item md={9} xs={12}>
-                    <Grid md={8} xs={12}>
+               <Grid container spacing={1} size={{md: 9, xs: 12}} >
+                    <Grid size={{md: 8, xs: 12}}>
                         <Box sx={{height: '300px', backgroundColor: '#222222', borderRadius:'15px'}}>
                             <Typography sx={{fontSize:'70px', pl:'25px'}}>About us</Typography> 
                             <hr style={{borderColor:'#FFFFFF', marginTop:'130px', opacity: 0.3}}></hr>
                             <Typography sx={{pl:'25px', fontSize: '18px', opacity:'0.8', marginTop:'15px'}}>Some numbers and stats about our team</Typography>
                         </Box>
                     </Grid>
-                    <Grid md={4} xs={12}>
+                    <Grid size={{md: 4, xs: 12}}>
                         <Box sx={{height: '300px', backgroundColor: '#DA3838', borderRadius:'15px'}}>
                             <Typography sx={{fontSize:'70px', pl:'25px'}}>50+</Typography>
                             <hr style={{borderColor:'#FFFFFF', marginTop:'130px', opacity: 0.3}}></hr>
                             <Typography sx={{pl:'25px', fontSize: '18px', opacity:'0.8', marginTop:'15px'}}>Members</Typography>
                         </Box>
                     </Grid>
-                    <Grid md={4} xs={12}>
-                        <Box sx={{borderRadius: '15px', overflow: 'hidden', height:'300px'}}>
-                            <img src={Image} style={{objectFit:'cover', width:'100%', height:'100%'}}></img>
+                    <Grid size={{md: 4, xs: 12}}>
+                        <Box sx={{borderRadius: '15px', 
+                                    overflow: 'hidden',
+                                    height:'300px',
+                                    position:'relative',
+                                    filter:'brightnes(1)',
+                                    '&:hover img':{
+                                        filter: 'brightness(0.7)',
+                                        transform: 'scale(1.05)'                                    }
+                                    }}>
+                            <img src={Image} style={{objectFit:'cover', width:'100%', height:'100%', transition: 'all 0.3s ease-in-out'}}></img>
+                            <Button text="Meet the team" style={{position:'absolute', top:'78%', left:'1%'}}/>
                         </Box>
                     </Grid>
-                    <Grid md sx>
+                    <Grid size={{md: 8, xs: 12}}>
                         <Box sx={{ 
                             borderRadius: '15px', 
                             border: 1, 
@@ -39,16 +50,16 @@ export default function About(){
                             alignItems: 'center',
                             paddingX: 2
                             }}>
-                            <Typography fontSize={20}>
+                            <Typography fontSize={24}>
                                 Our team culture is paramount. We are committed to inclusivity and empathy in everything we do. 
                                 We ensure that every member feels welcome, valued, and supported, fostering a collaborative and respectful environment.
                             </Typography>
                         </Box>
                     </Grid>
                 </Grid>
-                <Grid md={3} xs={12}>
-                    <Box container sx={{backgroundColor:'#FFFFFF', borderRadius:'15px', overflow: 'hidden', display:'flex', flexDirection:{md: 'column', xs:'row'}, padding: 1}}>
-                        <Grid md={12} xs={6}>
+                <Grid size={{md: 3, xs: 12}}>
+                    <Box alignItems={'center'} sx={{backgroundColor:'#FFFFFF', borderRadius:'15px', overflow: 'hidden', display:'flex', flexDirection:{md: 'column', xs:'row'}, padding: 1}}>
+                        <Grid size={{md: 8, xs: 12}}>
                             <PieChart
                                 series={[
                                     {
@@ -65,51 +76,49 @@ export default function About(){
                                 ]}
                                 slotProps={{legend:{hidden:true}}}
                                 width={300}
-                                height={200}
+                                height={207}
                             />
                         </Grid>
                         <Grid
                             container
+                            spacing={1.5}
+                            padding={2}
                             direction="column"
-                            wrap="wrap"
                             sx={{
-                                maxHeight: '400px',
+                                maxHeight: '500px',
                                 color: 'black',
-                                marginLeft:'5px',
-                                padding: 6.1,
                                 alignContent:'left',
                                 overflow:'auto'
                             }}
-                            md={12}
-                            xs={6}
+                            size={{md: 12, xs: 6}}
                             fontSize={20}
                             >
-                            <div style={{ margin: '5px', display: 'flex', alignItems: 'center' }}>
-                                <div style={{ width: '15px', height: '15px', backgroundColor: '#111111', marginRight: '5px' }}></div>
+                            <div style={{ margin: '5px', display: 'flex', alignItems: 'center'}}>
+                                <div style={{ borderRadius: 5, width: '30px', height: '30px', backgroundColor: '#111111', marginRight: '5px'}}></div>
                                 Science
                             </div>
                             <div style={{ margin: '5px', display: 'flex', alignItems: 'center' }}>
-                                <div style={{ width: '15px', height: '15px', backgroundColor: '#DDDDDD', marginRight: '5px' }}></div>
+                                <div style={{ borderRadius: 5, width: '30px', height: '30px', backgroundColor: '#DDDDDD', marginRight: '5px' }}></div>
                                 Arm
                             </div>
                             <div style={{ margin: '5px', display: 'flex', alignItems: 'center' }}>
-                                <div style={{ width: '15px', height: '15px', backgroundColor: '#DA3838', marginRight: '5px' }}></div>
+                                <div style={{ borderRadius: 5, width: '30px', height: '30px', backgroundColor: '#DA3838', marginRight: '5px' }}></div>
                                 Chassis
                             </div>
                             <div style={{ margin: '5px', display: 'flex', alignItems: 'center' }}>
-                                <div style={{ width: '15px', height: '15px', backgroundColor: '#414FE8', marginRight: '5px' }}></div>
+                                <div style={{ borderRadius: 5, width: '30px', height: '30px', backgroundColor: '#414FE8', marginRight: '5px' }}></div>
                                 ECS
                             </div>
                             <div style={{ margin: '5px', display: 'flex', alignItems: 'center' }}>
-                                <div style={{ width: '15px', height: '15px', backgroundColor: '#D9B638', marginRight: '5px' }}></div>
+                                <div style={{ borderRadius: 5, width: '30px', height: '30px', backgroundColor: '#D9B638', marginRight: '5px' }}></div>
                                 Logic
                             </div>
                             <div style={{ margin: '5px', display: 'flex', alignItems: 'center' }}>
-                                <div style={{ width: '15px', height: '15px', backgroundColor: '#CCCCCC', marginRight: '5px' }}></div>
+                                <div style={{ borderRadius: 5, width: '30px', height: '30px', backgroundColor: '#CCCCCC', marginRight: '5px' }}></div>
                                 Social Media
                             </div>
                             <div style={{ margin: '5px', display: 'flex', alignItems: 'center' }}>
-                                <div style={{ width: '15px', height: '15px', backgroundColor: '#AAAAAA', marginRight: '5px' }}></div>
+                                <div style={{ borderRadius: 5, width: '30px', height: '30px', backgroundColor: '#AAAAAA', marginRight: '5px' }}></div>
                                 Finance
                             </div>
                         </Grid>
